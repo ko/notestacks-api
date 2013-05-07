5baef965f0c239b3a178dcc23905fa9d7f76052a
Ken Ko <ko.harmony@gmail.com>
2013-05-07 06:09:28 +0000

    Saving works.
    Based on the postid, we can save the title and body of the note. This
    is independent of the stack id because we want to have posts transferable
    between stacks for reorganization purposes.

--- notestacks.py
+++ notestacks.py
@@ -39,7 +39,6 @@ def teardown_request(exception):
 # Helper functions
 #
 def fetchall_to_json_list_multiple(rows, list_name, list_entry, count):
-    import re
     data_list = []
     for row in rows:
         entry_dict = {}
@@ -50,7 +49,15 @@ def fetchall_to_json_list_multiple(rows, list_name, list_entry, count):
     data_final = {list_name : data_list}
     return data_final
 
-
+def fetchall_to_dict(rows, list_entry, count):
+    data_list = []
+    for row in rows:
+        entry_dict = {}
+        for i in range(0, count):
+            if row[i] is not None:
+                entry_dict[list_entry[i]] = row[i]
+        data_list.append(entry_dict)
+    return data_list
 
 #
 # Internal implementation of views
@@ -58,13 +65,22 @@ def fetchall_to_json_list_multiple(rows, list_name, list_entry, count):
 def _notes_list_get(type, to_find = None):
     import json
     if type == 'single':
-        cur = g.db.execute('select title, body from note_table order by id asc')
-        list_entry = ['title', 'body']
-        list_count = 2
+        cur = g.db.execute('select id, title, body from note_table order by id asc')
+        list_entry = ['id', 'title', 'body']
+        list_count = 3
     rows = cur.fetchall()
-    data_final = fetchall_to_json_list_multiple(rows, 'notes_list', list_entry, list_count);
+    data_final = fetchall_to_json_list_multiple(rows, 'notes_list', list_entry, list_count)
     json_data = json.dumps(data_final, encoding='utf-8', ensure_ascii=False)
     return json_data
+
+def _note_update(sid, nid, title, body):
+
+    query = 'UPDATE note_table '
+    query += 'SET stackid=?, title=?, body=? '
+    query += 'WHERE id=?;'
+    g.db.execute(query, [sid, title, body, nid])
+    g.db.commit()
+    return 'sup'
 
 #
 # Views
@@ -73,10 +89,28 @@ def _notes_list_get(type, to_find = None):
 def notes_index():
     return 'sup'
 
-@app.route('/api/list/')
+@app.route('/api/list/get/')
 #@crossdomain(origin='http://yaksok.net')
 def notes_list_get():
     return _notes_list_get('single')
+
+note_update = 1
+note_create = 2
+
+@app.route('/api/list/set/', methods = ['POST'])
+def notes_list_set():
+    type = int(request.form['type'])
+    if (type == note_update):
+        sid = request.form['stackid']
+        nid = request.form['noteid']
+        ntitle = request.form['title']
+        nbody = request.form['body']
+        _note_update(sid, nid, ntitle, nbody)
+    elif (type == note_create):
+        ntitle = request.form['title']
+        nbody = request.form['body']
+        stackid = int(request.form['stackid'])
+    return notes_list_get()
 
 @app.after_request
 def after_request(response):
